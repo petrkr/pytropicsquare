9727bb3eb069b53c079616f0d13e04fb5514e0ea
Petr Kracik <petrkr@petrkr.net>
2025-02-23 01:57:38 +0100

    More constants

--- tropicsquare/constants/__init__.py
+++ tropicsquare/constants/__init__.py
@@ -1,5 +1,14 @@
 
 
-# Format [Request ID, Request Length]
+# Format [Request ID, Optional request Length]
 
 REQ_ID_GET_INFO_REQ = [0x01, 0x02]
+REQ_ID_HANDSHARE_REQ = [0x02, 0x21]
+REQ_ID_ENCRYPTED_CMD_REQ = [0x04] # Dynamic length, send from API
+REQ_ID_ENCRYPTED_SESSION_ABT = [0x08, 0x00]
+REQ_ID_RESEND_REQ = [0x10, 0x00]
+REQ_ID_SLEEP_REQ = [0x20, 0x01]
+REQ_ID_STARTUP_REQ = [0xB3, 0x01]
+
+
+REQ_ID_GET_RESPONSE = [0xAA] # Does not send length
