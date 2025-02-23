817ee93f548ec20fa5f482ab734d46a92313e73c
Petr Kracik <petrkr@petrkr.net>
2025-02-23 16:30:13 +0100

    Check response status, try/except in test it

--- test.py
+++ test.py
@@ -10,13 +10,17 @@ def main():
     port = int(sys.argv[2])
 
     ts = TropicSquareNetworkSPI(host, port)
+
+    print("Spect FW version: {}".format(ts.spect_fw_version))
+    print("RISCV FW version: {}".format(ts.riscv_fw_version))
+    print("Chip ID: {}".format(ts.chipid))
+    try:
+        print("FW Bank: {}".format(ts.fw_bank))
+    except Exception as e:
+        print("Exception: {}".format(e))
+
     cert = ts.certificate
-    print(cert)
-
-    with open("tropic.crt", "wb") as f:
-        f.write(cert)
-
-    #print(ts.riscv_fw_version)
+    print("Certificate: {}".format(cert))
 
 
 if __name__ == "__main__":
