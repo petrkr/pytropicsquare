0bb95e502cac00b175272ee8324680b8d913ac1d
Petr Kracik <petrkr@petrkr.net>
2025-02-23 13:00:35 +0100

    Implemented micropython SPI logic

--- tropicsquare/ports/micropython.py
+++ tropicsquare/ports/micropython.py
@@ -8,3 +8,23 @@ class TropicSquareMicroPython(TropicSquare):
         self._gpo = gpo
 
         super().__init__()
+
+
+    def _spi_cs(self, value):
+        self._cs.value(value)
+
+
+    def _spi_write(self, data):
+        self._spi.write(data)
+
+
+    def _spi_read(self, len: int) -> bytes:
+        return self._spi.read(len)
+
+
+    def _spi_readinto(self, buffer: bytearray):
+        self._spi.readinto(buffer)
+
+
+    def _spi_write_readinto(self, tx_buffer, rx_buffer: bytearray):
+        self._spi.write_readinto(tx_buffer, rx_buffer)
