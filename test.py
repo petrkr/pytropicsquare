33d2a7d40c08519dfa15abf8e415cc0c589559a3
Petr Kracik <petrkr@petrkr.net>
2025-02-23 16:02:48 +0100

    Test use network SPI for now

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 
-from tropicsquare.ports.micropython import TropicSquareMicroPython
+import sys
+
+#from tropicsquare.ports.micropython import TropicSquareMicroPython
+from tropicsquare.ports.networkspi import TropicSquareNetworkSPI
 
 
 def main():
-    ts = TropicSquareMicroPython(None, None)
-    print(ts.chipid)
-    print(ts.riscv_fw_version)
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+
+    ts = TropicSquareNetworkSPI(host, port)
+    cert = ts.certificate
+    print(cert)
+
+    with open("tropic.crt", "wb") as f:
+        f.write(cert)
+
+    #print(ts.riscv_fw_version)
 
 
 if __name__ == "__main__":
