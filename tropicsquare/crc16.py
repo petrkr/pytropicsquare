8570f613cebf0e8e15fbdb7fdcfaddecf1ff2301
Petr Kracik <petrkr@petrkr.net>
2025-02-24 22:15:10 +0100

    Make CRC static classmethod, do not need instance

--- tropicsquare/crc16.py
+++ tropicsquare/crc16.py
@@ -1,32 +1,30 @@
 # Implement CRC16 checksum
 
-class CRC16:
+class CRC:
     CRC16_POLYNOMIAL = 0x8005
     CRC16_INITIAL_VAL = 0x0000
     CRC16_FINAL_XOR_VALUE = 0x0000
 
 
-    def __init__(self):
-        pass
+    @classmethod
+    def crc16(cls, data: bytes) -> bytes:
+        """Compute the CRC16 value for the given byte sequence."""
+        crc = cls.CRC16_INITIAL_VAL
+        for byte in data:
+            crc = cls._crc16_byte(byte, crc)
+        crc ^= cls.CRC16_FINAL_XOR_VALUE
+
+        return bytes([crc & 0xFF, (crc >> 8) & 0xFF])
 
 
-    def _crc16_byte(self, data: int, crc: int) -> int:
+    @classmethod
+    def _crc16_byte(cls, data: int, crc: int) -> int:
         """Process one byte of data into the CRC."""
         crc ^= data << 8
         for _ in range(8):
             if crc & 0x8000:
-                crc = (crc << 1) ^ self.CRC16_POLYNOMIAL
+                crc = (crc << 1) ^ cls.CRC16_POLYNOMIAL
             else:
                 crc <<= 1
             crc &= 0xFFFF
         return crc
-
-
-    def crc16(self, data: bytes) -> bytes:
-        """Compute the CRC16 value for the given byte sequence."""
-        crc = self.CRC16_INITIAL_VAL
-        for byte in data:
-            crc = self._crc16_byte(byte, crc)
-        crc ^= self.CRC16_FINAL_XOR_VALUE
-
-        return bytes([crc & 0xFF, (crc >> 8) & 0xFF])
