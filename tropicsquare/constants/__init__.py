37eedf20d2e62b4113bdda505553678875dd1f38
Petr Kracik <petrkr@petrkr.net>
2025-02-24 15:18:53 +0100

    Added some L3 CMD ID to constants

--- tropicsquare/constants/__init__.py
+++ tropicsquare/constants/__init__.py
@@ -1,6 +1,7 @@
 
 
 # Format [Request ID, Optional request Length]
+# L2 Request IDs
 
 REQ_ID_GET_INFO_REQ = [0x01, 0x02]
 REQ_ID_HANDSHARE_REQ = [0x02, 0x21]
@@ -16,3 +17,26 @@ REQ_ID_GET_RESPONSE = [0xAA] # Does not send length
 
 
 PROTOCOL_NAME = b'Noise_KK1_25519_AESGCM_SHA256\x00\x00\x00'
+
+# Format [Optional command Length, Command ID]
+# L3 Command IDs
+CMD_ID_PING = [0x01] # Dynamic length, send from API
+
+CMD_ID_PAIRING_KEY_WRITE = [0x24, 0x10]
+CMD_ID_PAIRING_KEY_READ = [0x03, 0x11]
+# CMD_ID_PAIRING_KEY_INVALIDATE = [0x03, 0x12] # For debug safe, do not support this yet
+
+# CMD_ID_R_CFG_WRITE = [0x08, 0x20] # For debug safe, do not support this yet
+CMD_ID_R_CFG_READ = [0x03, 0x21]
+# CMD_ID_R_CFG_ERASE = [0x01, 0x22] # For debug safe, do not support this yet
+
+# CMD_ID_I_CFG_WRITE = [0x04, 0x30] # For debug safe, do not support this yet
+CMD_ID_I_CFG_READ = [0x03, 0x31]
+
+CMD_ID_R_MEMDATA_WRITE = [0x40] # Dynamic length, send from API
+CMD_ID_R_MEMDATA_READ = [0x03, 0x41]
+CMD_ID_R_MEMDATA_ERASE = [0x03, 0x42]
+
+CMD_ID_RANDOM_VALUE = [0x02, 0x50]
+
+CMD_ECC_KEY_GENERATE = [0x04, 0x60]
