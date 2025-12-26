ec0f4bde8f6992d0ff6b41eb1b503d2d2a782c19
Petr Kracik <petrkr@petrkr.net>
2025-12-26 15:04:20 +0100

    Updated ESP32 hello world example

--- examples/esp32-hello.py
+++ examples/esp32-hello.py
@@ -1,22 +1,30 @@
 
-import sys
+"""ESP32 MicroPython Example for Tropic Square TROPIC01 Chip
+"""
 
-from tropicsquare.ports.micropython import TropicSquareMicroPython
-from tropicsquare.exceptions import *
 from machine import SPI, Pin
 
+from tropicsquare import TropicSquare
+from tropicsquare.transports.spi import SPITransport
+from tropicsquare.constants.pairing_keys import *
+from tropicsquare.exceptions import *
+
+
+pkey_index_0 = FACTORY_PAIRING_KEY_INDEX
 
 # Default factory pairing keys
-pkey_index_0 = 0x00 # Slot 0
-sh0priv = [0xd0,0x99,0x92,0xb1,0xf1,0x7a,0xbc,0x4d,0xb9,0x37,0x17,0x68,0xa2,0x7d,0xa0,0x5b,0x18,0xfa,0xb8,0x56,0x13,0xa7,0x84,0x2c,0xa6,0x4c,0x79,0x10,0xf2,0x2e,0x71,0x6b]
-sh0pub  = [0xe7,0xf7,0x35,0xba,0x19,0xa3,0x3f,0xd6,0x73,0x23,0xab,0x37,0x26,0x2d,0xe5,0x36,0x08,0xca,0x57,0x85,0x76,0x53,0x43,0x52,0xe1,0x8f,0x64,0xe6,0x13,0xd3,0x8d,0x54]
+# sh0priv = FACTORY_PAIRING_PRIVATE_KEY_ENG_SAMPLE
+# sh0pub  = FACTORY_PAIRING_PUBLIC_KEY_ENG_SAMPLE
+
+sh0priv = FACTORY_PAIRING_PRIVATE_KEY_PROD0
+sh0pub  = FACTORY_PAIRING_PUBLIC_KEY_PROD0
 
 
 def main():
     spi = SPI(1, baudrate=1_000_000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
     cs = Pin(5, Pin.OUT)
 
-    ts = TropicSquareMicroPython(spi, cs)
+    ts = TropicSquare(SPITransport(spi, cs))
 
     try:
         # Get chip information
@@ -34,7 +42,7 @@ def main():
 
         # Start secure session
         print("Starting secure session...")
-        ts.start_secure_session(pkey_index_0, bytes(sh0priv), bytes(sh0pub))
+        ts.start_secure_session(pkey_index_0, sh0priv, sh0pub)
         print("Secure session established!")
 
         # Test ping command
